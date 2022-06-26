39b178727550d87b27f8e1bfc3136bd63ea7078e
Rahul Upadhyay <rahulupadhyay@Rahuls-MacBook-Air.local>
2022-06-26 20:33:25 +0530

    added mocha awesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "test_report",
+    charts: true,
+    reportPageTitle: "Cypress POC",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
